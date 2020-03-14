116487b941c2c2504dfac8092910e9e52e228caf
Michael Lin <miclin@berkeley.edu>
2020-03-14 16:51:12 -0700

    Initial Commit

---
+++ Huddle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Huddle
+//
+//  Created by Michael Lin on 3/14/20.
+//  Copyright © 2020 Michael Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
